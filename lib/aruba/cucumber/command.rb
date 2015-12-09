ba8cd77ee1040f75e124f43d0f9163b9bc69920b
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-09 06:00:35 +0100

    change style of regex

--- lib/aruba/cucumber/command.rb
+++ lib/aruba/cucumber/command.rb
@@ -34,8 +34,8 @@ When(/I run the following script:$/) do |file_content|
   step 'I run `myscript`'
 end
 
-When(/^I run the following (bash|zsh|fish|dash)? ?commands?:$/) do |shell_type, file_content|
-  prepend_environment_variable('PATH', expand_path('bin') + ':')
+When(/^I run the following (?:(bash|zsh|fish|dash) )?commands?:$/) do |shell_type, file_content|
+  # prepend_environment_variable('PATH', expand_path('bin') + ':')
   shell_type ||= 'bash'
   step 'an executable named "bin/myscript" with:',
        "#!/usr/bin/env #{shell_type}
